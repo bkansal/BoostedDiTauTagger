0f3de1de9f06935e46fc703574cc08026948a9d1
SohamBhattacharya <soham.elessar@gmail.com>
2018-04-12 17:11:16 +0200

    Some updates

---
+++ generation/Common.h
@@ -0,0 +1,31 @@
+# ifndef Common_H
+# define Common_H
+
+
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <utility>
+# include <vector>
+
+# include <CLHEP/Vector/LorentzVector.h>
+
+
+namespace Common
+{
+    void parseFileByLine(std::string fileName, std::vector <std::string> *v_line)
+    {
+        std::ifstream lineStream;
+        lineStream.open(fileName);
+        
+        std::string line;
+        
+        while(std::getline(lineStream, line))
+        {
+            v_line->push_back(line);
+        }
+    }
+}
+
+
+# endif
